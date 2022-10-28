6d02339e30a81a1349260670b06337fc92d83951
Manuel Navarro <manuelnavarro2108@gmail.com>
2022-10-28 20:49:04 -0300

    Se creó el archivo logout.php

---
+++ logout.php
@@ -0,0 +1,16 @@
+9 lines (6 sloc)  105 Bytes
+
+<?php
+  session_start();
+
+  session_unset();
+
+  session_destroy();
+
+  ?>
+  <script>
+  /*  document.cookie="userid=; Secure; SameSite = Lax;";
+    window.location.href="index.php";
+    console.log("prueba");
+  */
+  </script>
